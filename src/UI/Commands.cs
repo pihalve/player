36f4272029fb8db28184c742909f78dd930616c2
Mads Larsen <pihalve@gmail.com>
2015-11-02 23:09:41 +0100

    create main window design

---
+++ src/UI/Commands.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace Pihalve.Player.UI
+{
+    public static class Commands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+            (
+                "E_xit",
+                "Exit",
+                typeof(Commands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.F4, ModifierKeys.Alt)
+                }
+            );
+    }
+}
---
+++ src/UI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pihalve.Player.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Defaults
+            MainGrid.ColumnDefinitions.First(c => c.Name == "NavigatorColumn").Width = new GridLength(30, GridUnitType.Star);
+            MainGrid.ColumnDefinitions.First(c => c.Name == "ContentColumn").Width = new GridLength(70, GridUnitType.Star);
+        }
+
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
