d933ec760e4c093a1fe12d4fb6dbb8cd017c9b83
Mads Larsen <pihalve@gmail.com>
2015-11-04 22:12:42 +0100

    adds model for Playlist

---
+++ src/UI/Library/Model/PlaylistItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pihalve.Player.Library.Model
+{
+    public class PlaylistItem : IComparable<PlaylistItem>
+    {
+        public int SortNumber { get; set; }
+        public Track Track { get; set; }
+
+        public int CompareTo(PlaylistItem other)
+        {
+            return SortNumber.CompareTo(other);
+        }
+    }
+}
